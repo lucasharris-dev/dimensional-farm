fa80f3047ced57505a08ea24cf9f2dc9d9a29e24
Lucas Harris <lucasharris.dev@outlook.com>
2023-05-21 13:21:19 -0400

    Add player input

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float walkSpeed = 10f;
 
-    
+    Vector2 moveInput;
+
+    Rigidbody2D myRigidbody;
+
     void Awake()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Start is called before the first frame update
@@ -20,6 +25,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Walk();
     }
+
+    void OnWalk(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void Walk()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * walkSpeed, moveInput.y * walkSpeed);
+        myRigidbody.velocity = playerVelocity;
+    }
+
 }
